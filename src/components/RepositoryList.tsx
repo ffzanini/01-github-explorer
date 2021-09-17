fd5e300e6c3ed23391eab645d66b28c498200009
ffzanini <devffzanini@gmail.com>
2021-09-17 11:49:33 -0300

    added typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,14 +3,13 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'uniform',
-  description: 'Form in React',
-  link: 'https://github.com/ffzanini'
+interface Repository {
+  name: string
+  description: string
+  html_url: string
 }
-
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(()=> {
     fetch('https://api.github.com/orgs/rocketseat/repos')
